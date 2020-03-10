f0ed2c04e63f645a8ec5db24f9c2fe039d0394be
JSU <nonexistent@jsu.edu>
2020-03-10 14:22:22 -0500

    Initial commit of layouts

---
+++ app/src/main/java/com/example/tablayoutdemo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.tablayoutdemo;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/tablayoutdemo/Tab2Fragment.java
@@ -0,0 +1,34 @@
+package com.example.tablayoutdemo;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.google.android.material.tabs.TabLayout;
+import androidx.appcompat.widget.*;
+import androidx.viewpager.widget.*;
+
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Tab2Fragment extends Fragment {
+
+
+    public Tab2Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab2, container, false);
+        
+    }
+
+}
